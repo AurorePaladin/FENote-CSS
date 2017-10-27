776cb17ef1990cd49054a7003bf2f0bc39af869d
AurorePaladin <aurorepaladin@gmail.com>
2017-10-27 23:28:49 +0800

    FENote ES6  let和const命令

---
+++ es6/let和const命令.js
@@ -0,0 +1,17 @@
+function test() {
+  let a=1;
+  //var a=2;
+  for(let i =1;i<3;i++){
+    console.log(i);
+  }
+  console.log(i);
+}
+function last(){
+  const PI=3.14159
+  const k={
+    a:1
+  }
+  k.b=3
+  console.log(PI,k);
+}
+last();
