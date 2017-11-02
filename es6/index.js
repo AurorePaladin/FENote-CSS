9ef0e58a51d9841767f381ff509c8cbb273d5e86
AurorePaladin <aurorepaladin@gmail.com>
2017-11-03 05:30:48 +0800

    FENote ES6 index.js

---
+++ es6/index.js
@@ -0,0 +1,21 @@
+// class Test {
+//   constructor() {
+//     this.a='hello world'
+//   }
+// }
+//
+// let test =new Test();
+//
+// document.body.innerHTML=test.a;
+//import './class/lesson1';
+//import './class/lesson2';
+//import './class/lesson3';
+// import 'babel-polyfill';
+// import './class/lesson16';
+// import {A,test,Hello} from './class/lesson17';
+// import {A} from './class/lesson17';
+// import * as lesson from './class/lesson17';
+// import lesson17 from './class/lesson17';
+// console.log(A,test,Hello);
+// console.log(lesson.A,lesson.test);
+// console.log(lesson.A);
